78dd56fabb3691e9c00519323dc824c46f46a038
Vladislav <vladislavfrontdev@gmail.com>
2022-06-20 18:51:30 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-dayNightTheme = () => {
-  let date = new Date();
-  let hour = date.getHours();
-
-  if (hour >= 7 && hour < 19) {
-    document.body.style.backgroundColor = "white";
-    document.body.style.color = "black";
-  } else {
-    document.body.style.backgroundColor = "black";
-    document.body.style.color = "white";
-  }
-};
-
-window.addEventListener("load", dayNightTheme);
 
 const input = document.querySelector("#input");
 const grid = document.querySelector(".grid");
